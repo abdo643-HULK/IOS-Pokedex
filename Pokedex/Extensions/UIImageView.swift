e9a9d450aa40a40f36bd454ff5dfef2247b746a2
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-06-27 09:27:42 +0200

    added async loading for images

---
+++ Pokedex/Extensions/UIImageView.swift
@@ -0,0 +1,24 @@
+//
+//  UIImageView.swift
+//  Pokedex
+//
+//  Created by Abdo on 19.06.21.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+	func load(url: URL) {
+		async {
+			do {
+				let (data, _) = try await URLSession.shared.data(from: url)
+				if let image = UIImage(data: data) {
+					self.image = image
+				}
+			} catch {
+				print(error.localizedDescription)
+			}
+		}
+	}
+}
